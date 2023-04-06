f2c1272b6908046e83e115828d59b4ff0b9e2bfb
Karol Marciniak <marciniakarol@gmail.com>
2023-04-06 19:18:53 +0200

    First commit - customer website layout

---
+++ Przychodnia.PortalWWW/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Przychodnia.PortalWWW.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
