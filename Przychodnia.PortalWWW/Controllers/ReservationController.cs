a3ce18b1fc090a0232242c6d9bf8a79da9a78393
Karol Marciniak <marciniakarol@gmail.com>
2023-05-15 21:19:03 +0200

    Add _ReservationLayout
    * New module for visit reservations
    * Divided by specialization (top panel)
    * General view of doctors and details

---
+++ Przychodnia.PortalWWW/Controllers/ReservationController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Przychodnia.Database.Data;
+
+namespace Przychodnia.PortalWWW.Controllers
+{
+    public class ReservationController : Controller
+    {
+        private readonly PrzychodniaContext _context;
+
+		public ReservationController(PrzychodniaContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<IActionResult> Index(int? id) //id - kliknięta specjalizacja
+		{
+			ViewBag.Specializations = await _context.Specialization.ToListAsync();
+
+			if (id == null)
+			{
+				var pierwszy = await _context.Specialization.FirstAsync();
+				id = pierwszy.Id;
+			}
+			return View(await _context.Doctor.Where(t => t.SpecializationId == id).ToListAsync());
+		}
+        public async Task<IActionResult> Details(int? id)
+        {
+            ViewBag.Specializations = await _context.Specialization.ToListAsync();
+            return View(await _context.Doctor.Where(t => t.Id == id).FirstOrDefaultAsync());
+
+        }
+    }
+}
