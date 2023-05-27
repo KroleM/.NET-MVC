b965060b4eb2d6991aa4eee85b20e5869d42ac14
Karol Marciniak <marciniakarol@gmail.com>
2023-05-27 10:24:54 +0200

    Add components in the Reservation menu
    * Component for Specialization choice
    * Improved Doctor Details view

---
+++ Przychodnia.PortalWWW/Controllers/SpecializationsMenuComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.EntityFrameworkCore;
+using Przychodnia.Database.Data;
+
+namespace Przychodnia.PortalWWW.Controllers
+{
+	public class SpecializationsMenuComponent : ViewComponent
+	{
+		private readonly PrzychodniaContext _context;
+		public SpecializationsMenuComponent(PrzychodniaContext context)
+		{
+			_context = context;
+		}
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			//do widoku SpecializationsMenuComponent przekazujemy wszystkie rodzaje z BD asynchronicznie (bez użycia ViewBagów)
+			return View("SpecializationsMenuComponent", await _context.Specialization.ToListAsync());
+		}
+	}
+}
