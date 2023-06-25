3e0f78031f33819c74782d3ad58b443b6442f6df
Karol Marciniak <marciniakarol@gmail.com>
2023-06-25 11:36:32 +0200

    Improved Reservation Calendar

--- Przychodnia.PortalWWW/Controllers/HomeController.cs
+++ Przychodnia.PortalWWW/Controllers/HomeController.cs
@@ -6,69 +6,87 @@ using System.Diagnostics;
 
 namespace Przychodnia.PortalWWW.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
-        private readonly PrzychodniaContext context_;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+		private readonly PrzychodniaContext context_;
 
-        public HomeController(PrzychodniaContext context)
-        {
-            context_ = context;
-        }
+		public HomeController(PrzychodniaContext context)
+		{
+			context_ = context;
+		}
 
-        public IActionResult Index(int? id)
-        {
-            ViewBag.PageModel =
-            (
-                    from page in context_.Page
-                    orderby page.Priority
-                    select page
-                ).ToList();
-            
-            if (id == null)
-            {
-                return View("MainPage");
-                //id = context_.Page.First().Id;
-            }
-            var item = context_.Page.Find(id);
+		public IActionResult Index(int? id)
+		{
+			ViewBag.PageModel =
+			(
+					from page in context_.Page
+					orderby page.Priority
+					select page
+				).ToList();
 
-            return View(item);
-        }
-        public IActionResult MainPage()
-        {
-            ViewBag.PageModel =
-            (
-                from page in context_.Page
-                orderby page.Priority
-                select page
-            ).ToList();
-            return View();
-        }
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-        public IActionResult Pricelist()
-        {
-            return View();
-        }
-        public IActionResult Facilities()
-        {
-            return View();
-        }
-        public IActionResult Doctors()
-        {
-            return View();
-        }
-        public IActionResult About()
-        {
-            return View();
-        }
+			if (id == null)
+			{
+				return View("MainPage");
+				//id = context_.Page.First().Id;
+			}
+			var item = context_.Page.Find(id);
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			return View(item);
+		}
+		public IActionResult MainPage()
+		{
+			ViewBag.PageModel =
+			(
+				from page in context_.Page
+				orderby page.Priority
+				select page
+			).ToList();
+			return View();
+		}
+		public IActionResult Privacy()
+		{
+			ViewBag.PageModel =
+			(
+				from page in context_.Page
+				orderby page.Priority
+				select page
+			).ToList();
+			return View();
+		}
+		public IActionResult Pricelist()
+		{
+			return View();
+		}
+		public IActionResult Facilities()
+		{
+			ViewBag.PageModel =
+			(
+				from page in context_.Page
+				orderby page.Priority
+				select page
+			).ToList();
+			return View();
+		}
+		public IActionResult Doctors()
+		{
+			return View();
+		}
+		public IActionResult About()
+		{
+			ViewBag.PageModel =
+			(
+				from page in context_.Page
+				orderby page.Priority
+				select page
+			).ToList();
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
